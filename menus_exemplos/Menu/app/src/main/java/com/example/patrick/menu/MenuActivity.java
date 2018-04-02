78ae38ae5a1a81e00fbf55d9ea8c2a53270f4a1a
Patrick Silva <trickpasp@users.noreply.github.com>
2018-04-02 09:11:30 -0300

    Subindo projeto

--- menus_exemplos/Menu/app/src/main/java/com/example/patrick/menu/MenuActivity.java
+++ menus_exemplos/Menu/app/src/main/java/com/example/patrick/menu/MenuActivity.java
@@ -1,9 +1,12 @@
 package com.example.patrick.menu;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActionBar;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
@@ -12,12 +15,31 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setOnQueryTextListener(onSearch());
         return true;
+    }
+
+    private SearchView.OnQueryTextListener onSearch() {
+        return new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                toast("Buscar o texto:" + query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        };
     }
 
     @Override
