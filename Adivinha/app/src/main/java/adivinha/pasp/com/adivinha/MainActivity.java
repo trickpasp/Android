232ad21bde8a2713df78f8ffaf200227f51bc35e
Patrick D. Silva <patrickeeep12@gmail.com>
2018-04-06 13:41:02 -0300

    Criando dimens, criando botões e alterando activity

--- Adivinha/app/src/main/java/adivinha/pasp/com/adivinha/MainActivity.java
+++ Adivinha/app/src/main/java/adivinha/pasp/com/adivinha/MainActivity.java
@@ -4,13 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoJogar = (Button) findViewById(R.id.botaoJogarId);
+        textoResultado = (TextView) findViewById(R.id.resultadoId);
+
+        textoResultado.setText("Texto Alterado");
+
+
     }
 
     @Override
