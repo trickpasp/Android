0f7b5a1320fcb4089f3d49338e6c6e9262c322fc
Patrick Silva <trickpasp@users.noreply.github.com>
2018-04-06 12:58:48 -0300

    Criando projeto adivinha

---
+++ Adivinha/app/src/main/java/adivinha/pasp/com/adivinha/MainActivity.java
@@ -0,0 +1,13 @@
+package adivinha.pasp.com.adivinha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
