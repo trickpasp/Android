3852b35804beae4278d2bca6c6842d288a812ccc
Patrick Silva <patrick.silva@multify.app>
2019-03-04 09:53:52 -0300

    Comentando código do frame layout

--- Frameecoordinatorlayout/app/src/main/java/com/cursoandroid/frameecoordinatorlayout/MainActivity.java
+++ Frameecoordinatorlayout/app/src/main/java/com/cursoandroid/frameecoordinatorlayout/MainActivity.java
@@ -1,20 +1,18 @@
 package com.cursoandroid.frameecoordinatorlayout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.FrameLayout;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FrameLayout frameLayout;
+//    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        frameLayout = findViewById(R.id.frameCarregando);
+//        frameLayout = findViewById(R.id.frameCarregando);
 //        frameLayout.setVisibility(View.GONE);
     }
 
