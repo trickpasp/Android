72b87f57b82b251c40603a277a7d1d7b3185cdcf
Patrick Silva <patrick.silva@multify.app>
2019-03-04 09:53:06 -0300

    Aplicando sobreposição de components

--- Frameecoordinatorlayout/app/src/main/java/com/cursoandroid/frameecoordinatorlayout/MainActivity.java
+++ Frameecoordinatorlayout/app/src/main/java/com/cursoandroid/frameecoordinatorlayout/MainActivity.java
@@ -2,12 +2,23 @@ package com.cursoandroid.frameecoordinatorlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameLayout = findViewById(R.id.frameCarregando);
+//        frameLayout.setVisibility(View.GONE);
     }
+
+    /*public void abrir(View view){
+        frameLayout.setVisibility(View.VISIBLE);
+    }*/
 }
